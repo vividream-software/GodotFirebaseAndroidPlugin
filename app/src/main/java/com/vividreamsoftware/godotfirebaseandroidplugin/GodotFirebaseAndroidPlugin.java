38baf8e5e50b375ddd2e1a94d323ec7d1c0a507e
Aaron Trudeau <120415438+trudeaua-vividream-software@users.noreply.github.com>
2023-04-22 22:19:19 -0400

    tweak implementation, fix firebase dependency nightmare

--- app/src/main/java/com/vividreamsoftware/godotfirebaseandroidplugin/GodotFirebaseAndroidPlugin.java
+++ app/src/main/java/com/vividreamsoftware/godotfirebaseandroidplugin/GodotFirebaseAndroidPlugin.java
@@ -1,8 +1,11 @@
 package com.vividreamsoftware.godotfirebaseandroidplugin;
 
+import android.content.Context;
 import android.util.ArraySet;
 
 import androidx.annotation.NonNull;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 import org.godotengine.godot.Godot;
 import org.godotengine.godot.plugin.GodotPlugin;
@@ -13,6 +16,8 @@ import java.util.List;
 import java.util.Set;
 
 public class GodotFirebaseAndroidPlugin extends GodotPlugin{
+    private FirebaseAnalytics mFirebaseAnalytics;
+
     public GodotFirebaseAndroidPlugin(Godot godot) {
         super(godot);
     }
@@ -26,7 +31,7 @@ public class GodotFirebaseAndroidPlugin extends GodotPlugin{
     @NonNull
     @Override
     public List<String> getPluginMethods() {
-        return Arrays.asList("getHello", "getHelloSignal");
+        return Arrays.asList("getHello", "getHelloSignal", "initializeFirebase");
     }
 
     @NonNull
@@ -40,6 +45,11 @@ public class GodotFirebaseAndroidPlugin extends GodotPlugin{
     public String getHello(){
         getHelloSignal("this is our test signal");
         return "Hello World";
+    }
+
+    public void initializeFirebase(){
+        // dingo
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this.getActivity());
     }
 
     public void getHelloSignal(String s){
